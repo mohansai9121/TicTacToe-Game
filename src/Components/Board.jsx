a7077f47f52c9c4d11156809b58fbe45389c62c1
Mohan Sai <mohansaichinthala9121@gmail.com>
2024-02-19 00:13:18 +0530

    Added two players and checked winner

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,19 +1,28 @@
 import Square from "./Square"
 import { useState } from "react"
 import './Styles.scss'
+import { checkWinner } from "./winner"
 
 const Board = () => {
   let [squares, setSquares] = useState(Array(9).fill(null))
-  console.log(squares)
+  let [isX, setIsX] = useState(true)
+  let player = isX?'X':'O'
+  let winnerIs = checkWinner(squares)
+  let status = winnerIs?`Winner is ${winnerIs}`:`Now, its ${player} turn`
+  //console.log(squares)
   const squareHandler = (position)=>{
+    if(squares[position] || winnerIs){
+      return;
+    }
     setSquares((gameSquares)=>{
       return gameSquares.map((squareValue, p)=>{
         if(position === p){
-          return 'x'
+          return isX? 'X': 'O'
         }
         return squareValue
       })
     })
+    setIsX((valueX)=>!valueX)
   }
 
   const renderSquares = (pos)=>{
@@ -24,6 +33,10 @@ const Board = () => {
 
   return (
     <div className="board">
+      <div>
+        <h2>Tic Tac Toe</h2>
+        <h3>{status}</h3>
+      </div>
       <div className="board-row">
         {renderSquares(0)}
         {renderSquares(1)}
